aeb32e6d3858c45f5ca9794e08ce3493fc06d8d8
sarthakpokharel <sarthakpokharel2004@gmail.com>
2024-12-03 19:22:03 -0500

    add

--- index.js
+++ index.js
@@ -1,44 +1,39 @@
-import "dotenv/config";
-import express from "express";
-import Hello from "./hello.js";
+import express from 'express';
+import Hello from "./Hello.js"
 import Lab5 from "./Lab5/index.js";
 import cors from "cors";
 import UserRoutes from "./Kanbas/Users/routes.js";
-import session from "express-session";
 import CourseRoutes from "./Kanbas/Courses/routes.js";
 
+import session from "express-session";
+import "dotenv/config";
 const app = express();
 
 app.use(
-  cors({
-    credentials: true,
-    origin: process.env.NETLIFY_URL || "http://localhost:3000",
-  })
-);
-app.use(express.json());
-
+    cors({
+      credentials: true,
+      origin: process.env.NETLIFY_URL || "http://localhost:3000",
+    })
+   );
+app.use(express.json());   
 const sessionOptions = {
-  secret: process.env.SESSION_SECRET || "kanbas",
-  resave: false,
-  saveUninitialized: false,
-};
-
-if (process.env.NODE_ENV !== "development") {
-  sessionOptions.proxy = true;
-  sessionOptions.cookie = {
-    sameSite: "none",
-    secure: true,
-    domain: process.env.NODE_SERVER_DOMAIN,
+    secret: process.env.SESSION_SECRET || "kanbas",
+    resave: false,
+    saveUninitialized: false,
   };
-}
-app.use(session(sessionOptions));
-
-// Routes
+  if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+      domain: process.env.NODE_SERVER_DOMAIN,
+    };
+  }
+  app.use(session(sessionOptions));
+  
+Hello(app);
+Lab5(app);
 UserRoutes(app);
 CourseRoutes(app);
-Lab5(app);
-Hello(app);
 
-app.listen(process.env.PORT || 4000, () => {
-  console.log("Server is running on port", process.env.PORT || 4000);
-});
+app.listen(process.env.PORT || 4000)
